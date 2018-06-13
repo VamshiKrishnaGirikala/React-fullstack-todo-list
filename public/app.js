4d99b74575ff47187ac095529d2e3998925c1f2e
vamshi krishna girikala <vamshikrishna.girikala@gmail.com>
2018-06-12 22:24:11 -0400

    app completed

--- public/app.js
+++ public/app.js
@@ -6,18 +6,13 @@ $(document).ready(function(){
         if(event.which==13){
             createTodo();
         }
+    });
+    $('.list').on('click','li',function(){
+         updateTodo($(this));
     })
-    $('.list').on('click','span',function(){
-        var clickedId=$(this).parent().data('id');
-        var deleteUrl='/api/todos/'+clickedId;
-        $(this).parent().remove();
-        $.ajax({
-            method:'DELETE',
-            url:deleteUrl
-        })
-            .then(function(data){
-                console.log(data);
-            })
+    $('.list').on('click','span',function(e){
+        e.stopPropagation()
+        removeTodo($(this).parent());
     })
 });
 
@@ -42,9 +37,39 @@ function createTodo(){
 function addTodo(todo){
     var newTodo=$("<li class='task'>"+todo.name+"<span>X</span></li>");
     newTodo.data('id',todo._id);
+    newTodo.data("completed",todo.completed);
     if(todo.completed){
         newTodo.addClass("done")
     }
 
     $(".list").append(newTodo);
 };
+function removeTodo(todo){
+    var clickedId=todo.data('id');
+    var deleteUrl='/api/todos/'+clickedId;
+
+    $.ajax({
+        method:'DELETE',
+        url:deleteUrl
+    })
+        .then(function(data){
+            todo.remove();
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+}
+function updateTodo(todo){
+    var updateUrl='/api/todos/'+todo.data('id');
+    var isDone=!todo.data('completed');
+    var updateData={completed:isDone}
+$.ajax({
+    method:'PUT',
+    url:updateUrl,
+    data:updateData
+})
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data('completed',isDone)
+    })
+}
