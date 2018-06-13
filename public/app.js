9aa7bf4ec240ad482bdaafa24f8b56fcd0415385
vamshi krishna girikala <vamshikrishna.girikala@gmail.com>
2018-06-12 21:55:10 -0400

    delete functionality completed

--- public/app.js
+++ public/app.js
@@ -7,21 +7,44 @@ $(document).ready(function(){
             createTodo();
         }
     })
+    $('.list').on('click','span',function(){
+        var clickedId=$(this).parent().data('id');
+        var deleteUrl='/api/todos/'+clickedId;
+        $(this).parent().remove();
+        $.ajax({
+            method:'DELETE',
+            url:deleteUrl
+        })
+            .then(function(data){
+                console.log(data);
+            })
+    })
 });
+
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+
+addTodo(todo);
+    })
+}
+function createTodo(){
+    var usrInput=$('#todoInput').val();
+   $.post("/api/todos",{name:usrInput})
+       .then(function(newTodo){
+           $('#todoInput').val('')
+    addTodo(newTodo);
+       })
+       .catch(function(err){
+           console.log(err)
+       })
+}
 function addTodo(todo){
-    var newTodo=$("<li class='task'>"+todo.name+"</li>");
+    var newTodo=$("<li class='task'>"+todo.name+"<span>X</span></li>");
+    newTodo.data('id',todo._id);
     if(todo.completed){
         newTodo.addClass("done")
     }
 
     $(".list").append(newTodo);
-}
-function addTodos(todos){
-    todos.forEach(function(todo){
-
-
-    })
-}
-function createTodo(){
-    var usrInput=$('#todoInput').val();
-   $.post("/api/todos",{name:usr
+};
